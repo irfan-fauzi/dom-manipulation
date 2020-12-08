b324a6c9e9ebb669cbad945efc0483f2c8d4f5be
irfan-fauzi <irfanwintech@yahoo.com>
2020-12-08 21:32:34 +0700

    make function to reduce repetitive code

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const lisectionB = document.querySelector("section#b ul li:nth-child(1)");
 const semuaLi = document.querySelectorAll("section#b li");
 const sectionB = document.querySelector("section#b ul");
 
+
+
 // end element ---------------------
 
 // start function ------------------
@@ -20,7 +22,7 @@ function toggleClass(el, className) {
   return el.classList.toggle(className);
 }
 
-// ADD ELEMENT
+// ADD ELEMENT to PARENT
 function addEl(el, textNode, parent) {
   const newEl = document.createElement(el);
   const nodeEl = document.createTextNode(textNode);
@@ -30,12 +32,29 @@ function addEl(el, textNode, parent) {
 
 // addEl('li', 'saya gila', sectionB);
 
-// Remove element
+// REMOVE ELEMEN
 function removeEl(child, parent) {
   return parent.removeChild(child);
 }
 // removeEl(lisectionB, sectionB);
 
+
+// REPLACE ELEMEN
+function replaceEL(parent, oldElemen, newElemen) {
+  return parent.replaceChild(newElemen, oldElemen)
+}
+
+//replaceEL(sectionB, lisectionB, newLi)
+function buatElemenNode(node, text) {
+  const elBaru = document.createElement(node);
+  const textBaru = document.createTextNode(text);
+  elBaru.appendChild(textBaru);
+  return elBaru
+}
+
+//const buat = buatElemenNode("p", "waaau");
+
+replaceEL(sectionB, lisectionB, buatElemenNode("p", "waaau"))
 
 
 
